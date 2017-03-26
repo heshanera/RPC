90b7db062e198de0b67430047b4660b365818723
Heshan Eranga <erangamx@gmail.com>
2017-03-26 21:25:58 +0530

    Add files via upload

--- server.c
+++ server.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "prog.h"
 
 int * search_1_svc(operands *argp, struct svc_req *rqstp)
 {
-	static int  similar = 0;
+	static int similar = 0;
+	similar -= similar;
 	printf("Got request: searching '%c' in '%s'\n", argp->c, argp->line);
-	while(*(argp->line) != '\0')
+	int len = strlen(argp->line);
+	//printf("%d\n", len);
+	int i = 0;
+	while(i < len)
 	{
-		//printf("%c\n",*line);
-		if ( *(argp->line) == argp->c ) similar++;
-		(argp->line)++;
-	}	
+		//printf("%c\n",(argp->line)[i]);
+		if ( (argp->line)[i] == argp->c ) similar++;
+		i++;
+	}
+		
 	return (&similar);
 }
